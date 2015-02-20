b45b910bfd00230d1f4ed7fbef88e436ee0d9efd
J. TURNER <turner.j6@gmail.com>
2015-02-20 13:38:34 -0800

    Update js to include functionality for customer name.

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,46 @@ var pizzaCo = {
   slices: 0,
 
     pizza: function() {
+      var name = this.name;
       var description = this.description;
       var size = this.size;
       var slices = this.slices;
       var falseDescription = (description !== "Cheese"
-                              || !== "Pepperoni");
-      var falseSize = (size !== "8" || "12" || "16"
+                              || "Pepperoni");
+      var falseSize = (size "8" || "12" || "16"
                        || isNaN(size));
 
-      if (faultyDescription) {
+      if (falseDescription) {
         alert("Our two pizza types are CHEESE and PEPPERONI.");
+      } else {
+        "<li>" + description + "</li>";
       };
-      if (faultySize) {
+
+      if (falseSize) {
         alert("Our three pizza sizes are 8-inch, 12-inch,
                 and 16-inch.");
+      } else {
+        "<li>" + size + "</li>";
       };
 
     };
 
+});
+
+$(document).ready(function() {
+  $("form#new-order").submit(function(event) {
+    event.preventDefault();
+
+    pizza.description = $(this).find("input#description").val();
+    pizza.size = parseInt($(this).find("input#size").val());
+    var newPizza = { description: };
+
+    $("input#description").val("");
+    $("input#size").val("");
+
+    $(".description-result").show();
+    $(".size-result").show();
+
+
+  });
 });
