a4652842233f292b220c1af84181019d12e37e13
J. TURNER <turner.j6@gmail.com>
2015-02-20 10:44:50 -0800

    Add alerts for faulty input regarding description and size.

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,19 @@ var pizzaCo = {
       var description = this.description;
       var size = this.size;
       var slices = this.slices;
+      var falseDescription = (description !== "Cheese"
+                              || !== "Pepperoni");
+      var falseSize = (size !== "8" || "12" || "16"
+                       || isNaN(size));
+
+      if (faultyDescription) {
+        alert("Our two pizza types are CHEESE and PEPPERONI.");
+      };
+      if (faultySize) {
+        alert("Our three pizza sizes are 8-inch, 12-inch,
+                and 16-inch.");
+      };
+
     };
 
 });
