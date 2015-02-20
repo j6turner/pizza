3c497e7ee733043087c25b309162f653abd99543
J. TURNER <turner.j6@gmail.com>
2015-02-20 10:05:33 -0800

    Establish third spec.

--- spec/specs.js
+++ spec/specs.js
@@ -11,11 +11,11 @@ describe("pizzaCo", function() {
       expect(pizza.size).to.equal("8");
   });
 
-  // it("returns number of slices", function() {
-  //   var pizza = { description: "Cheese",
-  //                   size: "8",
-  //                    slices: "6" };
-  //     expect(pizza.slices).to.equal("6")
-  // });
+  it("returns number of slices", function() {
+    var pizza = { description: "Cheese",
+                    size: "8",
+                     slices: "6" };
+      expect(pizza.slices).to.equal("6")
+  });
 
 });
