82c2593f482b096c5bd567d7e71acda17c87ae4f
J. TURNER <turner.j6@gmail.com>
2015-02-20 13:37:17 -0800

    Add spec for customer name.

--- spec/specs.js
+++ spec/specs.js
@@ -1,20 +1,28 @@
 describe("pizzaCo", function() {
 
+  it("returns customer name", function() {
+    var pizza = { name: "Vivica Vance" };
+      expect(pizza.name).to.equal("Vivica Vance");
+  });
+
   it("returns type of pizza", function() {
-    var pizza = { description: "Cheese" };
+    var pizza = { name: "Vivica Vance",
+                  description: "Cheese" };
       expect(pizza.description).to.equal("Cheese");
   });
 
   it("returns size of pizza", function() {
-    var  pizza = { description: "Cheese",
-                     size: "8"};
+    var  pizza = { name: "Vivica Vance",
+                   description: "Cheese",
+                   size: "8" };
       expect(pizza.size).to.equal("8");
   });
 
   it("returns number of slices", function() {
-    var pizza = { description: "Cheese",
-                    size: "8",
-                     slices: "6" };
+    var pizza = { name: "Vivica Vance",
+                  description: "Cheese",
+                  size: "8",
+                  slices: "6" };
       expect(pizza.slices).to.equal("6")
   });
 
